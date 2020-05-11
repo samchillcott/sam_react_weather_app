57d7e74b0eb143b4646194b27e32a4b666c2ce0b
Sam Chillcott <samchillcott@gmail.com>
2020-05-11 12:23:54 +0100

    bsaic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import weather from "./weather";
+import "./App.css";
 
-function App() {
-  return (
-    null
-  );
-}
+const App = () => {
+	const API_KEY = "e55f6512fa859d8ffa450609930032ad";
 
+	const [weather, setWeather] = useState([]);
+	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState("");
+
+	const getWeather = async () => {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
+		);
+		const data = await response.json();
+		setWeather(data.hits);
+		console.log(data.hits);
+	};
+
+	useEffect(() => {
+		getWeather();
+		// eslint-disable-next-line
+	}, [query]);
+
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	};
+
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch("");
+	};
+
+	return (
+		<div className="App">
+			<h1 className="title">Weather App</h1>
+      <form onSubmit={getSearch} className="search-form">
+				<input
+					className="search-bar"
+					type="text"
+					placeholder="City name"
+					value={search}
+					onChange={updateSearch}
+				/>
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
+      <div className="weather">
+			  {/* {<weather />} */}
+		  </div>
+    </div>  
+	);
+};
 export default App;
---
+++ src/weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./weather.module.css";
+
+const weather = () => {
+	return (
+		<div className={style.weather}>
+			<p>Weather Component Text</p>
+		</div>
+	);
+};
+
+export default weather;
