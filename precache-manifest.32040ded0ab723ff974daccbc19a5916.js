373b80eb1903b4939f6190fa8b77022669aede01
Sam Chillcott <samchillcott@gmail.com>
2020-05-18 12:02:30 +0100

    Updates

---
+++ precache-manifest.32040ded0ab723ff974daccbc19a5916.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786c03be036ec37ce4644cb2859e8ff9",
+    "url": "/sam_react_weather_app/index.html"
+  },
+  {
+    "revision": "eb5dcfd1f6fc3d533387",
+    "url": "/sam_react_weather_app/static/css/main.e13b1845.chunk.css"
+  },
+  {
+    "revision": "d97b8076cb791cf69781",
+    "url": "/sam_react_weather_app/static/js/2.dc3456ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sam_react_weather_app/static/js/2.dc3456ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5dcfd1f6fc3d533387",
+    "url": "/sam_react_weather_app/static/js/main.d845089a.chunk.js"
+  },
+  {
+    "revision": "86d1eb313910b201678d",
+    "url": "/sam_react_weather_app/static/js/runtime-main.226d5873.js"
+  },
+  {
+    "revision": "0404e2e184d5d6058b2c1b44e55cd602",
+    "url": "/sam_react_weather_app/static/media/defaultbg.0404e2e1.jpg"
+  },
+  {
+    "revision": "97345d718148c39876bdb7f0361a9996",
+    "url": "/sam_react_weather_app/static/media/jack.97345d71.jpg"
+  },
+  {
+    "revision": "87148278371f45cd89b2f380de06fa47",
+    "url": "/sam_react_weather_app/static/media/non_sunny.87148278.jpg"
+  },
+  {
+    "revision": "50449f5c97f121de3b004448f74ecf80",
+    "url": "/sam_react_weather_app/static/media/sunny.50449f5c.jpg"
+  }
+]);
